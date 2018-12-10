151493cb7026c0e4de4bcddd6310867213e2d983
adamjc <adamjc86@gmail.com>
2018-12-10 14:46:54 +0000

    Initial day-1 implementation

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+
+use std::fs::File;
+use std::io::prelude::*;
+use std::collections::HashMap;
+use std::boxed::Box;
+
+fn main() -> std::io::Result<()> {
+    let mut file = File::open("./src/input.txt")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    
+    let ops = make_ops();
+
+    Ok(())
+}
+
+fn make_ops () -> HashMap<String, Box<Fn(i32, i32) -> i32>> {
+  let mut ops: HashMap<String, Box<Fn(i32, i32) -> i32>> = HashMap::new();
+
+  ops.insert("+".into(), Box::new(add));
+  ops.insert("-".into(), Box::new(sub));
+
+  return ops
+}
+
+fn add (x: i32, y: i32) -> i32 {
+  return x + y;
+}
+
+fn sub (x: i32, y: i32) -> i32 {
+  return x - y;
 }

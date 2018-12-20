4ca468b7bf67326b010a5a8721978f530206d37e
adamjc <adamjc86@gmail.com>
2018-12-20 13:47:56 +0000

    Fix day_2

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -6,20 +6,25 @@ fn main() -> Result<()> {
 
     let exact_digits_count = buffer.split("\n")
                   .map(|id| find_exacts(id))
-                  .collect::<Vec<(i32, i32)>>()
-                  .into_iter()
-                  .fold(vec![0, 2], |mut acc, next| {
-                    acc[0] += next.0;
-                    acc[1] += next.1;
-                    
-                    acc
-                  });
+                  .collect::<Vec<(i32, i32)>>();
 
-    let checksum = exact_digits_count[0] * exact_digits_count[1];
+    let checksum = checksum(&exact_digits_count);
 
     println!("{:?}", checksum);
 
     Ok(())
+}
+
+fn checksum(vec: &Vec<(i32, i32)>) -> i32 {
+  let exact_digits_count = vec.into_iter()
+     .fold(vec![0; 2], |mut acc, next| {
+       acc[0] += next.0;
+       acc[1] += next.1;
+
+       acc
+     });
+
+  exact_digits_count[0] * exact_digits_count[1]
 }
 
 // find any digits that match exactly twice or thrice
@@ -29,7 +34,7 @@ fn find_exacts (id: &str) -> (i32, i32) {
   let mut char_counts = vec![0; 26];
 
   for chr in id.chars() {
-    let index = (chr.to_digit(36).unwrap() as usize) - 10;    
+    let index = (chr.to_digit(36).unwrap() as usize) - 10;
     char_counts[index] += 1;
   }
 
@@ -42,6 +47,8 @@ fn find_exacts (id: &str) -> (i32, i32) {
       thrice = 1;
     }
   }
+
+  println!("{:?} contains {:?} twice and {:?} thrice", id, twice, thrice);
 
   (twice, thrice)
 }

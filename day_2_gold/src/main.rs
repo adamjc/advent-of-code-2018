27030ffb6f1412479f455a754fe994f4c7ff4872
adamjc <adamjc86@gmail.com>
2018-12-21 11:50:03 +0000

    Add almost_dupe fn for &str

--- day_2_gold/src/main.rs
+++ day_2_gold/src/main.rs
@@ -1,13 +1,31 @@
 use std::io::{Read, Result, stdin};
 
 trait Duplicate {
-  fn find_dupes(&self, other: &str) -> &str;
+  fn is_almost_duplicate(&self, other: &str) -> bool;
 }
 
 impl Duplicate for &str {
-  fn find_dupes(&self, other: &str) -> &str {
-    // Go through each character, find how many dupes...
-    "Not yet implemented"
+  // -> true when exactly one character difference between strings, in exact index
+  fn is_almost_duplicate(&self, other: &str) -> bool {
+    let mut number_of_dupes = other.len();
+
+    for (i, _) in self.char_indices() {
+      let other_char = other.get(i..i + 1);
+      let this_char = self.get(i..i + 1);
+
+      if this_char != other_char {
+        number_of_dupes -= 1;
+      }
+    }
+
+    println!("self: {:?}", self);
+    println!("other: {:?}", other);
+
+    if number_of_dupes == other.len() - 1 {
+      return true
+    }
+
+    false
   }
 }
 
@@ -25,7 +43,13 @@ fn main() -> Result<()> {
       let next_chars = next_id.chars();
       let id_chars = id.chars();
 
-      println!("{:?}", next_id.find_dupes("Hello"));
+      let is_almost_dupe = next_id.is_almost_duplicate(first);
+
+      if is_almost_dupe {
+        println!("{:?}", next_id);
+        println!("{:?}", first);
+      }
+
     }
   }
 
